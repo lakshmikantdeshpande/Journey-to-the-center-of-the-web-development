d9941bf14caa5dd4aa92eca022c6ccb9392e32ab
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2018-07-11 21:29:28 +0530

    Arrow functions in angular

---
+++ angular/Angular Course by Mosh/misc/ArrowFunctions.js
@@ -0,0 +1,5 @@
+var log = function (message) {
+    console.log(message);
+};
+// kinda like lambda expression
+var doLog = function (message) { return console.log(message); };
---
+++ angular/Angular Course by Mosh/misc/ArrowFunctions.ts
@@ -0,0 +1,7 @@
+let log = function(message) {
+	console.log(message);
+}
+
+
+// kinda like lambda expression
+let doLog = (message) => console.log(message);
