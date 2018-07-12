4596a1fc7fb31756790e530dbd352cb6d5984304
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2018-07-12 22:33:56 +0530

    Interfaces

---
+++ angular/Angular Course by Mosh/misc/Interfaces.js
@@ -0,0 +1,4 @@
+var drawPoint = function (point) {
+    console.log(point.x);
+    console.log(point.y);
+};
---
+++ angular/Angular Course by Mosh/misc/Interfaces.ts
@@ -0,0 +1,9 @@
+interface Point {
+	x: number, 
+	y: number
+}
+
+let drawPoint = (point: Point) => {
+	console.log(point.x);
+	console.log(point.y);
+}
