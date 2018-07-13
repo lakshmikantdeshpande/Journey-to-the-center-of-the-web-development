5bec0827dd98aafda5be781e2f33b27ff6dc3d68
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2018-07-13 23:45:24 +0530

    Learnt about constructors

---
+++ angular/Angular Course by Mosh/misc/Constructor.ts
@@ -0,0 +1,17 @@
+class Point {
+	x: number;
+	y: number;
+
+	constructor(x: number, y: number) {
+		this.x = x;
+		this.y = y;
+	}
+
+	draw() {
+		console.log('x: ' + this.x + 'y: ' + this.y);
+	}
+}
+
+let point = new Point(2, 3);
+point.draw();
+
