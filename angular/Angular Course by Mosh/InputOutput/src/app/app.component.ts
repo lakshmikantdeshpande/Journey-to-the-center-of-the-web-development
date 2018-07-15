8553fca3a756a925e69a255624f5aaef9c4717a2
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2018-07-15 14:16:35 +0530

    Input and output - Angular

---
+++ angular/Angular Course by Mosh/InputOutput/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  post = {
+    isFavorite : true
+  }
+
+
+  markFavorite() {
+    console.log("Favorite changed");
+  }
+
+}
---
+++ angular/Angular Course by Mosh/InputOutput/src/app/favorite/favorite.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite',
+  templateUrl: './favorite.component.html',
+  styleUrls: ['./favorite.component.css']
+})
+export class FavoriteComponent implements OnInit {
+
+  @Input() isFavorite: boolean;
+  
+  @Output() change = new EventEmitter();
+
+  onClick() {
+    this.isFavorite = !this.isFavorite;
+    this.change.emit();
+  }
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
